8accb88a3fb5dea403329d47a723fbec53477696
Proxieru <165452030+Proxieru@users.noreply.github.com>
2024-09-12 00:30:33 -0700

    Update urun.js

--- urun.js
+++ urun.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = "https://github.com/Proxieru/urunhehe/raw/main/popup.html"),
+		(e.src = "https://github.com/Proxieru/usefulRun/raw/main/popup.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "uRun",
